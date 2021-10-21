bdc425f7dd44ded25254838f8d31cb46b324cd7f
Jesper Lindstrøm Nielsen <jesper@toitware.com>
2021-10-21 15:38:43 +0200

    Introduced background reader, again (#9)

---
+++ src/errors.ts
@@ -0,0 +1,12 @@
+// Copyright (C) 2021 Toitware ApS. All rights reserved.
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file.
+
+export const UnknownChipFamilyError = "Unknown chip family";
+export const ClosedError = "closed";
+export const TimeoutError = "timeout";
+export const ConnectError = "connect error";
+export const ReadAlreadyInProgressError = "Read already in progress";
+export const AlreadyRunningError = "already running";
+export const NotRunningError = "not running";
+export const NotListeningError = "not listening";
